48598444d6ef36b83eed10188d5dfccab7c4b79d
Afshin <mnafshin@gmail.com>
2018-09-12 17:48:06 +0200

    First commit

---
+++ src/main/java/org/aksw/conversiontool/ConversionToolApplication.java
@@ -0,0 +1,12 @@
+package org.aksw.conversiontool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversionToolApplication.class, args);
+    }
+}
